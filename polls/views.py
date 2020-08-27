9ac4e0ef6ed99aebf97a532608326f810869690d
Rafael Amaral <rafael.amaral@eximia.co>
2020-08-27 16:06:00 -0300

    Correção do req e adiciona wrapper

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from .flags import feature_flag_client
 
+
 # def index(request):
 #     if feature_flag_client.is_enabled('release'):
 #         return JsonResponse({'status' : 'success', 'flag': 'release'})
@@ -29,14 +30,63 @@ from .flags import feature_flag_client
 #         return JsonResponse({'status' : 'failure', 'flag': 'clientIds', 'userId': payload['userId']})
 
 
+# def index(request):
+#     payload = {'custom_parameter' : request.GET['typeOfBike']}  
+#     if feature_flag_client.is_enabled("custom_strategy_toggle", payload):
+#         return JsonResponse({'status' : 'success', 'flag': 'custom_flexible', 'custom_parameter': payload['custom_parameter']})
+#     else:
+#         return JsonResponse({'status' : 'failure', 'flag': 'custom_flexible', 'custom_parameter': payload['custom_parameter']})
+
+# Abstraction for Flags - Deve hospedar todas as Feature Toggle associado a feature de Greetings.
+class IGreetingFlags:
+    def isReleaseEnable():
+        pass
+
+
+class GreetingFlags(IGreetingFlags):
+    def isReleaseEnable(self):
+        return feature_flag_client.is_enabled('release')
+
+
+class AlwaysFailureFeature:
+    def handle(self):
+        return {
+            'status': 'failure',
+            'flag': 'flag',
+        }
+
+
+class AlwaysSuccessFeature:
+    def handle(self):
+        return {
+            'status': 'success',
+            'flag': 'flag',
+        }
+
+
+class GreetingFeaturesHandlersFactory:
+    def __init__(self, flags):
+        self.flags = flags
+
+    def createHandler(self):
+        if (self.flags.isReleaseEnable()):
+            return AlwaysSuccessFeature()
+        else:
+            return AlwaysFailureFeature()
+
+
+class GreetingService:
+
+    def __init__(self, factoryHandler):
+        self.factoryHandler = factoryHandler
+
+    def execute(self):
+        handler = self.factoryHandler.createHandler()
+        return handler.handle()
+
+
+service = GreetingService(GreetingFeaturesHandlersFactory(GreetingFlags()))
+
+
 def index(request):
-    payload = {'custom_parameter' : request.GET['typeOfBike']}  
-    if feature_flag_client.is_enabled("custom_strategy_toggle", payload):
-        return JsonResponse({'status' : 'success', 'flag': 'custom_flexible', 'custom_parameter': payload['custom_parameter']})
-    else:
-        return JsonResponse({'status' : 'failure', 'flag': 'custom_flexible', 'custom_parameter': payload['custom_parameter']})
-
-
-
-        
-    
+    return JsonResponse(service.execute())
